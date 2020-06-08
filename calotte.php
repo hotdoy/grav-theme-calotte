0edf60a61d0287f4c74974b5edbd80bb2b946d51
Hotdoy <rouleau.vincent@gmail.com>
2020-06-08 09:04:08 -0400

    back to un inherited theme! (MAJOR CHANGE)

--- calotte.php
+++ calotte.php
@@ -3,7 +3,14 @@ namespace Grav\Theme;
 
 use Grav\Common\Theme;
 
-class Calotte extends Calottecore
+class Calotte extends Theme
 {
     // Access plugin events in this class
+
+	public function onTwigSiteVariables()
+    {
+        if ($this->isAdmin()) {
+            $this->grav['assets']->add('theme://css/admin.css');
+        }
+    }
 }
