574b9016be7c04683c75fa6df6b9fa242f5e46fb
Hotdoy <rouleau.vincent@gmail.com>
2020-06-29 11:25:41 -0400

    improved header and header js as anonymous function

--- js/header.js
+++ js/header.js
@@ -1,37 +1,45 @@
-const	b = document.body;
-const onTriggers = document.getElementsByClassName("header-panel__on");
-const offTriggers = document.getElementsByClassName("header-panel__off");
-let lastOffset = 0;
+(function () {
+	
+	'use strict';
 
-for (let onTrigger of onTriggers) {
-	onTrigger.addEventListener("click", function(){
-		b.classList.add("header-panel--on");
-	})
-}
+	const InitHeader = function() {
+		const b = document.body;
+		const onTriggers = document.getElementsByClassName("header-panel__on");
+		const offTriggers = document.getElementsByClassName("header-panel__off");
+		let lastOffset = 0;
 
-for (let offTrigger of offTriggers) {
-	offTrigger.addEventListener("click", function(){
-		b.classList.remove("header-panel--on");
-	})
-}
+		for (let onTrigger of onTriggers) {
+			onTrigger.addEventListener("click", function(){
+				b.classList.add("header-panel--on");
+			})
+		}
 
-window.addEventListener('resize', function(){
-	if (window.innerWidth > 735 && b.classList.contains("header-panel--on")) {
-		b.classList.remove("header-panel--on");
+		for (let offTrigger of offTriggers) {
+			offTrigger.addEventListener("click", function(){
+				b.classList.remove("header-panel--on");
+			})
+		}
+
+		window.addEventListener('resize', function(){
+			if (window.innerWidth > 735 && b.classList.contains("header-panel--on")) {
+				b.classList.remove("header-panel--on");
+			}
+		});
+
+		window.addEventListener("scroll", function(){
+
+		   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
+
+		   	if (currentOffset < lastOffset && window.pageYOffset > 200){
+				b.classList.add("header-onscroll--on"); 
+		   	} else if (currentOffset > (lastOffset + 10) || window.pageYOffset < 200) {
+		    	b.classList.remove("header-onscroll--on");
+		   	}
+
+		   	lastOffset = currentOffset;
+		}, false);
 	}
-});
 
-window.addEventListener("scroll", function(){
+	InitHeader();
 
-   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
-
-   	if (currentOffset < lastOffset && window.pageYOffset > 200){
-		b.classList.add("header-onscroll--on"); 
-   	} else if (currentOffset > (lastOffset + 10) || window.pageYOffset < 200) {
-    	b.classList.remove("header-onscroll--on");
-   	}
-
-   	lastOffset = currentOffset;
-}, false);
-
-
+})();
